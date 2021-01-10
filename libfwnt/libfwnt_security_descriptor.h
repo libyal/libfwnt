b4a75eaacf73834fa964dc29668c70f06caa4913
Joachim Metz <joachim.metz@gmail.com>
2021-01-10 07:12:22 +0100

    Applied updates

--- libfwnt/libfwnt_security_descriptor.h
+++ libfwnt/libfwnt_security_descriptor.h
@@ -1,7 +1,7 @@
 /*
  * Windows NT Security Descriptor functions
  *
- * Copyright (C) 2009-2020, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2021, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
