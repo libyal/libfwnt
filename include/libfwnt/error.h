0be76ffeaea33e25a99b6c5e56adbaf0d2e0797d
Joachim Metz <joachim.metz@gmail.com>
2026-06-23 06:00:57 +0200

    Worked on tests and changes for CI tests

--- include/libfwnt/error.h
+++ include/libfwnt/error.h
@@ -82,7 +82,7 @@ enum LIBFWNT_ARGUMENT_ERROR
 	 */
 	LIBFWNT_ARGUMENT_ERROR_UNSUPPORTED_VALUE	= 8,
 
-	/* The argument contains a value that conficts with another argument
+	/* The argument contains a value that conflicts with another argument
 	 */
 	LIBFWNT_ARGUMENT_ERROR_CONFLICTING_VALUE	= 9
 };
@@ -214,7 +214,7 @@ enum LIBFWNT_OUTPUT_ERROR
 {
 	LIBFWNT_OUTPUT_ERROR_GENERIC			= 0,
 
-	/* There is insuficient space to write the output
+	/* There is insufficient space to write the output
 	 */
 	LIBFWNT_OUTPUT_ERROR_INSUFFICIENT_SPACE		= 1
 };
